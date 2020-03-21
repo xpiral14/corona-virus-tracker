d9cbf4959190b5f90a4e593f323becb68d07a963
xpiral14 <sa.reis1999@gmail.com>
2020-03-21 14:16:45 -0300

    Adicionado configurações e urls globais

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+const config = {
+  rapidApi: {
+    apiKey: "6361f46cfbmsh9a37d3f8bde97a2p115d2ejsnfe349b24f61e",
+    apiHost: "coronavirus-monitor.p.rapidapi.com"
+  },
+  urls: {
+    affectedCountries:
+      "https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php",
+    historyByParticularCountry: country =>
+      `https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_particular_country.php?country=${country}`,
+    casesByCountry: `https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php`,
+    latestStateByCountry: country =>
+      `https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=${country}`,
+    worldTotalState: `https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php`
+  }
+};
+
+export default config;
