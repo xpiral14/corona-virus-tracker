03b0dad4ea20e6a1d7c3958fbee84642b45ae136
xpiral14 <sa.reis1999@gmail.com>
2020-03-24 04:16:59 -0300

    Modificado gráfico para ser responsível

--- src/components/CountryGraphic/index.js
+++ src/components/CountryGraphic/index.js
@@ -7,34 +7,36 @@ import {
   AreaChart,
   LineChart,
   Legend,
-  Line
+  Line,
+  ResponsiveContainer
 } from "recharts";
 
 export default function CountryGraphic({ data, lineColor, dataKey, dataKeyX }) {
+  console.log(lineColor)
   const xAndYColor = "#212121";
   return (
-    <AreaChart
-      width={300}
-      height={200}
-      data={data}
-      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-    >
-      <defs>
-        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor={"green"} stopOpacity={0.8} />
-          <stop offset="95%" stopColor={"red"} stopOpacity={0} />
-        </linearGradient>
-      </defs>
-      <XAxis dataKey={dataKeyX} stroke={xAndYColor} />
-      <YAxis stroke={xAndYColor} />
-      <Tooltip contentStyle={{ stroke: "#212121" }} />
-      <Area
-        type="monotone"
-        dataKey={dataKey}
-        stroke={lineColor}
-        fillOpacity={1}
-        fill="url(#colorUv)"
-      />
-    </AreaChart>
+    <ResponsiveContainer width="100%" height={250}>
+      <AreaChart
+        data={data}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor={lineColor} stopOpacity={0.8} />
+            <stop offset="95%" stopColor={lineColor} stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey={dataKeyX} stroke={xAndYColor} />
+        <YAxis stroke={xAndYColor} />
+        <Tooltip contentStyle={{ stroke: "#212121" }} />
+        <Area
+          type="monotone"
+          dataKey={dataKey}
+          stroke={lineColor}
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
   );
 }
